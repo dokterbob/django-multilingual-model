353e1af05bd135c84dfa05b05c7e0dced1dd5478
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-02 13:57:27 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#from distribute_setup import use_setuptools
+#use_setuptools()
+
+from setuptools import setup, find_packages
+
+try:
+    README = open('README.rst').read()
+except:
+    README = None
+
+#try:
+#    REQUIREMENTS = open('requirements.txt').lines()
+#except:
+#    REQUIREMENTS = None
+
+setup(
+    name='django-multilingual-model',
+    version='0.1',
+    description='Django Simple Multilingual Support for Models.',
+    long_description=README,
+    #install_requires=REQUIREMENTS,
+    author='Mathijs de Bruin',
+    author_email='mathijs@mathijsfietst.nl',
+    url='http://github.com/dokterbob/django-multilingual-model',
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
