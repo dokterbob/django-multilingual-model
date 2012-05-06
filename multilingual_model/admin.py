d01a13f498e01efe613bace4c140d6901752475b
Tarak Blah <halbkarat@googlemail.com>
2012-05-06 17:32:16 +0300

    Rename TranslationInline to TranslationStackedInline, add TranslationTabularInline.

--- multilingual_model/admin.py
+++ multilingual_model/admin.py
@@ -4,9 +4,22 @@ from .forms import TranslationFormSet
 from . import settings
 
 
-class TranslationInline(admin.StackedInline):
+class TranslationStackedInline(admin.StackedInline):
     def __init__(self, *args, **kwargs):
-        super(TranslationInline, self).__init__(*args, **kwargs)
+        super(TranslationStackedInline, self).__init__(*args, **kwargs)
+
+        if settings.AUTO_HIDE_LANGUAGE:
+            self.exclude = ('language_code', )
+            self.can_delete = False
+
+    extra = 1
+    formset = TranslationFormSet
+    max_num = MAX_LANGUAGES
+
+
+class TranslationTabularInline(admin.TabularInline):
+    def __init__(self, *args, **kwargs):
+        super(TranslationTabularInline, self).__init__(*args, **kwargs)
 
         if settings.AUTO_HIDE_LANGUAGE:
             self.exclude = ('language_code', )
