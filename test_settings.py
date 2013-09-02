fe69f762da499f8a5771e1ad8504bf59a55ff97c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-09-02 16:30:44 +0200

    Infrastructure for convenient logging during tests.

--- test_settings.py
+++ test_settings.py
@@ -17,3 +17,27 @@ except ImportError:
     pass
 
 SITE_ID = 1
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'simple': {
+            'format': '- %(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+    }
+}
